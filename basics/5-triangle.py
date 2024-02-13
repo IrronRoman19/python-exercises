df3a9e904fb3a70c66c3bd53828f3ff5332adf72
Roman Fesunenko <roman.fesunenko@gmail.com>
2024-02-13 14:43:05 +0200

    added exercise 5

---
+++ basics/5-triangle.py
@@ -0,0 +1,10 @@
+# Exercise 5 - Triangle
+
+base = int(input("Enter the base length: "))
+height = int(input("Enter the height length: "))
+
+area = int(0.5 * (base * height))
+
+print("Base length:", base)
+print("Height length:", height)
+print("Area of triangle is:", area)
