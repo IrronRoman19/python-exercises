7c159c6490aa53e29a218024df9edb6288567617
Roman Fesunenko <roman.fesunenko@gmail.com>
2024-02-13 14:34:54 +0200

    added exercise 4

---
+++ basics/4-celsius-to-fahrenheit.py
@@ -0,0 +1,8 @@
+# Exercise 4 - Celsius or fahrenheit
+
+celsius = int(input("Enter celsius temperature: "))
+
+fahrenheit = int((1.8 * celsius) + 32)
+
+print("Temperature in Celsius:", celsius)
+print("Temperature in Fahrenheit:", fahrenheit)
