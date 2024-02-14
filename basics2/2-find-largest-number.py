46d6102f72a94ce4ca94285ebd01f012946a846f
Roman Fesunenko <roman.fesunenko@gmail.com>
2024-02-14 13:42:22 +0200

    updated condition to exercise 2

--- basics2/2-find-largest-number.py
+++ basics2/2-find-largest-number.py
@@ -5,7 +5,9 @@ list_1 = [5, 10, 60, 9, 12, 53, 120, 53, 12, 64]
 max_result = 0
 
 for i in list_1:
-    if i > max_result: 
+    if i > max_result:
         max_result = i
+    else:
+        pass
 
 print(f"Largest number in a list: {max_result}")
