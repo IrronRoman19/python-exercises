68bbf5db7f3884d3845bdfed587e4dc9b3436ae4
Roman Fesunenko <roman.fesunenko@gmail.com>
2024-02-14 11:50:03 +0200

    updated exercise if statement

--- basics2/if-statement2.py
+++ basics2/if-statement2.py
@@ -2,6 +2,8 @@
 
 num = int(input("Enter the number: "))
 
+# Option 1
+
 if (num % 5 == 0) and (num % 7 == 0) and (1500 <= num <= 2700):
     print(f"Yes! is number {num}")
 else:
@@ -11,3 +13,13 @@ else:
         print(f"No! {num} is not divided by 7")
     if not(1500 <= num <= 2700):
         print(f"No! {num} is not in ranges between 1500 to 2700")
+
+# Option 2
+
+# if (num % 35 == 0) and (1500 <= num <= 2700):
+#     print(f"Yes! is number {num}")
+# else:
+#     if not(num % 35 == 0):
+#         print(f"No! {num} is not divided by 35")
+#     if not(1500 <= num <= 2700):
+#         print(f"No! {num} is not in ranges between 1500 to 2700")
