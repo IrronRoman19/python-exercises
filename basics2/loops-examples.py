64cdbb8be876ef0ef048d6d166d6c7d5e35952da
Roman Fesunenko <roman.fesunenko@gmail.com>
2024-02-14 13:08:15 +0200

    updated exercises

---
+++ basics2/loops-examples.py
@@ -0,0 +1,33 @@
+n = 1
+
+while n < 10:
+    print("Hello!")
+    n += 1
+
+# while 1 == 1:
+#     print("Hello!")
+#     n += 1
+    
+# List array
+list_1 = ["Roman", "Alexander", "Leonard", "Nick", "Martin"]
+i = 0
+
+len_list_1 = len(list_1)
+print(len_list_1)
+
+print("~" * 20)
+
+# While loop
+while i != len_list_1:
+    print(list_1[i])
+    i += 1
+
+print("*Printing done*")
+
+print("~" * 20)
+
+# For loop
+for ex in list_1:
+    print(ex)
+
+print("*Printing done*")
